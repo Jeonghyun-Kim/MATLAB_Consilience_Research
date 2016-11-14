bf13b20a5a3d8db7121ba1bc42070191428e1e32
kukdh1 <kukdh1@gmail.com>
2016-11-15 01:25:11 +0900

    dot counting rotation fix

--- ART Files/ART.m
+++ ART Files/ART.m
@@ -1,16 +1,17 @@
 %% clc & clear
 clc;
 clear;
-% close all;
+close all;
 
 %% Initial Settings
-iter_num = 5;
-N = 512;
+iter_num = 6;
+N = 256;
 view = 301;
 theta = 0:180/view:180 - 180/view;
 
 dot_interval = 0.1;
 ray_interval = 1;
+relaxation_factor = 2;
 let = 1;
 DS_Factor = 5;
 
@@ -33,24 +34,18 @@ for iter = 1:iter_num
     view_count = 0;
     for th = theta .* pi ./180
         view_count = view_count + 1;
-        proj_filtered = (proj(:, view_count) - DotCounting(img_iter, dot_interval, ray_interval, N, th * 180/pi, let))./proj_weight(:, view_count)./iter_num;
+        proj_filtered = (proj(:, view_count) - DotCounting(img_iter, dot_interval, ray_interval, N, th * 180/pi, let))./proj_weight(:, view_count)/relaxation_factor;
         recon = interp1(AXIS, proj_filtered', Y .* sin(th) + X .* cos(th), 'linear');
         index = find((isnan(recon) == 1));
         recon(index) = 0;
         img_iter = img_iter + recon;
+        index = find (img_iter < 0);
+        img_iter(index) = 0;
     end
     A{iter} = img_iter;
 end
 
-for i = 1:iter_num
-    temp = A{i};
-    index = find(temp < 0);
-    temp(index) = 0;
-    A{i} = temp;
-end
-
 %% Figuring
-% close all;
 for i = 1:iter_num
     figure;
     imshow(A{i}, []);
