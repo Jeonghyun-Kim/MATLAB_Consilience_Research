58e5974a2edfcc921a3f961e19610f23088d80ce
Jeonghyun Kim <kimjh@bawi.org>
2016-11-15 10:56:04 +0900

    echo on; figure using subplots add relaxation factor delete useless files in 'ART_files' directory.

--- ART Files/ART.m
+++ ART Files/ART.m
@@ -4,9 +4,11 @@ clear;
 close all;
 
 %% Initial Settings
+echo on;
+
 iter_num = 6;
 N = 256;
-view = 301;
+view = 101;
 theta = 0:180/view:180 - 180/view;
 
 dot_interval = 0.1;
@@ -22,11 +24,15 @@ AXIS = (- N/2 + 0.5) * ray_interval:ray_interval:(N/2 - 0.5) * ray_interval;
 
 %% Prepare Images
 % ph = phantom(N);
+tic;
 ph_DS = CustomPhantom(N, DS_Factor);
+toc;
 img_weight = ones(N);
 proj_weight = DotCounting(img_weight, dot_interval, ray_interval, N, theta, let);
 proj = DotCounting(ph_DS, dot_interval, ray_interval, N, theta, let);
 img_iter = zeros(N);
+
+echo off;
 
 %% Iterative Reconstruction
 for iter = 1:iter_num
@@ -34,7 +40,8 @@ for iter = 1:iter_num
     view_count = 0;
     for th = theta .* pi ./180
         view_count = view_count + 1;
-        proj_filtered = (proj(:, view_count) - DotCounting(img_iter, dot_interval, ray_interval, N, th * 180/pi, let))./proj_weight(:, view_count)/relaxation_factor;
+        proj_filtered = (proj(:, view_count) - DotCounting(img_iter, dot_interval, ray_interval, N, th * 180/pi, let))...
+            ./proj_weight(:, view_count)/relaxation_factor;
         recon = interp1(AXIS, proj_filtered', Y .* sin(th) + X .* cos(th), 'linear');
         index = find((isnan(recon) == 1));
         recon(index) = 0;
@@ -48,6 +55,15 @@ end
 %% Figuring
 for i = 1:iter_num
     figure;
+    imshow(A{i}, []);
+    str = sprintf('iteration %d', i);
+    title(str);
+end
+
+%% Figuring together (6 iterations)
+figure;
+for i = 1:6
+    subplot(2, 3, i);
     imshow(A{i}, []);
     str = sprintf('iteration %d', i);
     title(str);
